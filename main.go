52b689dc5232ecafe0fbbe49035ca8958d201197
Olena Melnyk <Olena.Melnyk@capitalcct.ac.uk>
2023-08-30 19:01:33 +0100

    FetchAllRecipes

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Mriyaaa/RecipeKeeper/data"
 	"github.com/Mriyaaa/RecipeKeeper/handlers"
 )
 
 func main() {
+	data.FetchAllRecipes()
+	fmt.Println(data.AllRecipes)
 	http.HandleFunc("/", handlers.HomePage)
 	http.ListenAndServe(":8000", nil)
 }
